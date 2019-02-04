d80dcd5ccdd201c4ce3f748aff4c05447090e8b7
dpzem <deanp.95@gmail.com>
2019-02-05 09:10:12 +1100

    Telstra Buttons
    Added Telstra themed buttons

--- src/Components/Button/button.js
+++ src/Components/Button/button.js
@@ -1,13 +1,74 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 
-const myButton = props => {
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit
+  },
+  bootstrapRoot: {
+    boxShadow: "none",
+    textTransform: "none",
+    fontSize: 20,
+    padding: "6px 12px",
+    border: "1px solid",
+    lineHeight: 1.5,
+    backgroundColor: "#025450 ",
+    borderColor: "#025450 ",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(","),
+    "&:hover": {
+      backgroundColor: "#0069d9",
+      borderColor: "#0062cc"
+    },
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#0062cc",
+      borderColor: "#005cbf"
+    },
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)"
+    }
+  }
+});
+
+function CustomizedButtons(props) {
+  const { classes } = props;
+
   return (
-    <span>
-      <Button color="primary">{props.label}</Button>
-    </span>
+    <div>
+      <Button
+        variant="contained"
+        color="primary"
+        disableRipple
+        style={{
+          minWidth: 200,
+          maxWidth: 200,
+          minHeight: 70,
+          maxHeight: 200
+        }}
+        className={classNames(classes.margin, classes.bootstrapRoot)}
+      >
+        {props.label}
+      </Button>
+    </div>
   );
+}
+
+CustomizedButtons.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-export default myButton;
+export default withStyles(styles)(CustomizedButtons);
