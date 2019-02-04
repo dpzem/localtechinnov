f7a02f9f083dd6d8d551b91c0dda0688a112dc06
AjinkyaKhairnar <47285884+AjinkyaKhairnar@users.noreply.github.com>
2019-02-05 10:10:07 +1100

    Multiple charts in DataVis
    Ajinkya

--- src/Views/DataVis.js
+++ src/Views/DataVis.js
@@ -23,6 +23,26 @@ let data = {
   labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 };
 
+let data2 = {
+  datasets: [
+    {
+      label: "Number of Days",
+      data: [10, 38, 24, 28],
+      backgroundColor: [
+        "rgba(255, 78, 132, 0.8)",
+        "rgba(54, 162, 246, 0.8)",
+        "rgba(255, 206, 0, 0.8)",
+        "rgba(128, 0, 120, 0.8)"
+      ],
+      borderWidth: 2,
+      borderColor: "#777",
+      hoverBorderWidth: 3,
+      hoverBorderColor: "#000"
+    }
+  ],
+  labels: ["Area 1", "Area 2", "Area 3", "Area 4"]
+};
+
 class DataVis extends Component {
   constructor(props) {
     super(props);
@@ -41,32 +61,92 @@ class DataVis extends Component {
   render() {
     return (
       <div>
-        <h1> DataVis </h1>
-        <Bar
-          data={data}
+        <Pie
+          data={data2}
           width="100"
           height="33"
           options={{
+            responsive: true,
+            title: {
+              display: true,
+              text: "On average, where was my team seated this month?",
+              fontFamily: "Calibri",
+              fontSize: 25,
+              fontColor: "#359"
+            },
+            legend: {
+              display: true,
+              labels: {
+                fontSize: 16,
+                fontFamily: "Calibri"
+              }
+            },
+            rotation: 0,
+            animation: {
+              animateRotate: true,
+              animateScale: true
+            }
+            /*tooltips: {
+              callbacks: {
+                label: function(tooltipItem, data2) {
+                  var dataset = data2.datasets[tooltipItem.datasetIndex];
+                  var meta = dataset._meta[Object.keys(dataset._meta)[0]];
+                  var total = meta.total;
+                  var currentValue = dataset.data2[tooltipItem.index];
+                  var percentage = parseFloat(
+                    ((currentValue / total) * 100).toFixed(1)
+                  );
+                  return currentValue + " (" + percentage + "%)";
+                },
+                title: function(tooltipItem, data2) {
+                  return data2.labels[tooltipItem[0].index];
+                }
+              }
+            }*/
+          }}
+        />
+        <Bar
+          data={data}
+          width="100"
+          height="50"
+          options={{
+            responsive: true,
             scales: {
               yAxes: [
                 {
                   ticks: {
                     beginAtZero: true,
                     min: 0,
-                    max: 50
+                    max: 50,
+                    fontSize: 14
+                  }
+                }
+              ],
+              xAxes: [
+                {
+                  ticks: {
+                    fontSize: 16,
+                    fontFamily: "Calibri",
+                    fontStyle: "Bold"
                   }
                 }
               ]
             },
             title: {
               display: true,
-              text: "Total WFH Days (Last 30 Days)",
+              text: "How many days did my team work from home this month?",
               fontFamily: "Calibri",
               fontSize: 25,
               fontColor: "#359"
             },
             legend: {
               display: false
+            },
+            layout: {
+              padding: {
+                top: 50,
+                left: 0
+              }
             }
           }}
         />
