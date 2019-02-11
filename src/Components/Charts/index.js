17a018024e5872cd4898a856c167b1d6c40525c6
AjinkyaKhairnar <47285884+AjinkyaKhairnar@users.noreply.github.com>
2019-02-11 14:02:04 +1100

    Charts Update

--- src/Components/Charts/index.js
+++ src/Components/Charts/index.js
@@ -1,22 +1,29 @@
 import React from "react";
-import Grid from '@material-ui/core/Grid';
-import Chart1 from './Chart1';
-import Chart2 from './Chart2';
-import Chart3 from './Chart3';
-import Chart4 from './Chart4';
+import Grid from "@material-ui/core/Grid";
+import Chart1 from "./Chart1";
+import Chart2 from "./Chart2";
+import Chart3 from "./Chart3";
+import Chart4 from "./Chart4";
 
 const styles = {
-  Paper: { padding: 25, marginTop: 10, marginBottom: 10, marginLeft: 10, marginRight: 10}
-}
+  Paper: {
+    padding: 15,
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 5,
+    marginRight: 5
+  }
+};
 
-export default props =>
+export default props => (
   <Grid container>
-    <Grid item sm >
+    <Grid item sm>
       <Chart1 styles={styles} />
       <Chart2 styles={styles} />
     </Grid>
-    <Grid item sm >
+    <Grid item sm>
       <Chart3 styles={styles} />
       <Chart4 styles={styles} />
     </Grid>
   </Grid>
+);
