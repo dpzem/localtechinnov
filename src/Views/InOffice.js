7f2c1ef6aa2131b523bafa7d6a9f036e3fcb703c
Daniel de Waard <danieldewaard@users.noreply.github.com>
2019-02-11 15:58:21 +1100

    Bubble layered over image
    Bubble component created and layered over image.

--- src/Views/InOffice.js
+++ src/Views/InOffice.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Button from "../Components/Button/button";
+import Bubble from "../Components/Bubble/bubble";
 import myImage from "../Assets/Pictures/Pacific-RGB.png";
 import telstraLogo from "../Assets/Pictures/Logo.png";
 import floorplan from "../Assets/Pictures/level7.jpg";
 
 class AtOffice extends Component {
   render() {
+    console.log("test");
+
     return (
       <div
         style={{
@@ -13,7 +16,7 @@ class AtOffice extends Component {
           backgroundPosition: "center",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
-          height: "100vh"
+          height: "100%"
         }}
       >
         <img
@@ -23,33 +26,38 @@ class AtOffice extends Component {
             width: 80,
             margin: 10
           }}
+          alt="floor plan"
         />
         <center>
-          <br /> <br />
           <h1> Where in the office are you?</h1>
           <br />
+
           <div
+            className="imageContainer"
             style={{
+              textAlign: "center",
               position: "relative",
-              content: ".",
-              display: "block",
-              clear: "both",
-              fontSize: 0
+              display: "inline-block",
+              maxWidth: "50%",
+              zoom: 1
             }}
           >
-            <img
-              src={floorplan}
-              style={{
-                maxHeight: "100%",
-                maxWidth: "100%",
-                height: "70%",
-                width: "70%",
-                border: "3px solid",
-                margin: 10
-              }}
-            />
+            <div>
+              <Bubble />
+              <img
+                src={floorplan}
+                border="3"
+                style={{
+                  display: "block",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  maxWidth: "100%"
+                }}
+              />
+            </div>
           </div>
         </center>
+        <br />
       </div>
     );
   }
